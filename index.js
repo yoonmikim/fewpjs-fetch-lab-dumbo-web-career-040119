be01c1050904c932ed369902aeb3784bc9ce1fbc
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-17 14:27:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-function fetchBooks() {
-  return fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json())
-  .then(json => renderBooks(json));
+function fetchBooks(){
+  return fetch("https://anapioficeandfire.com/api/books"
+     ).then(resp => resp.json()).then(json => renderBooks(json));
 }
+
+// function fetchBooks() {
+//   return fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp => resp.json())
+//   .then(json => renderBooks(json));
+// }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
